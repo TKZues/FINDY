63163fb72948a18a207be3a533fd4c56e838352f
TKZues <137180128+TKZues@users.noreply.github.com>
2023-12-21 14:50:14 +0700

    Update photoconfirm.php

--- photoconfirm.php
+++ photoconfirm.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="icon" href="./img/logoicon.jpg" sizes="6x6">
     <link rel="stylesheet" href="./manageroder.css">
-    <link rel="stylesheet" href="./base.css">
+    <link rel="stylesheet" href="./css/base.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+<<<<<<< HEAD
 <?php
     include "id_tho.php";
 ?>
@@ -74,5 +77,326 @@
                         
                     
 </div>
+=======
+    <div class="content">
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar1.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Vũ Thành Danh
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Quận 1, TP.Hồ Chí Minh
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093345</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh cổ trang</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>300.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar2.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Nguyễn Thị Yến Nhi
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Quận 3, TP.Hồ Chí Minh
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093346</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh sinh nhật</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>1.000.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar3.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Võ Văn Luân
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Quận 10, TP.Hồ Chí Minh
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093347</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh đám cưới</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>8.000.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar9.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Phạm Yến Như
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Studio Wiwi
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093348</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh mẹ và bé</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>500.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar6.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Nguyễn Bảo Hân
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Quận 9, TP.Hồ Chí Minh
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093348</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh hiện đại</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>500.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col l-12 m-12 c-12">
+            <div class="order">
+                <div class="order__avatar">
+                    <img src="./img/avatar8.jpg" alt="Ảnh đại diện" class="order__avatar-img">
+                </div>
+
+                <div class="order__info">
+                    <span class="order__name">
+                        Bùi Thị Yến Nhi
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-location-dot"></i>
+                        Bình Dương
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-solid fa-calendar-days"></i>
+                        11/12/2023
+                    </span>
+                    <span class="order__text">
+                        <i class="order__text-icon fa-regular fa-clock"></i>
+                        16h:30p
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        Mã đơn hàng:
+                        <strong>0093350</strong>
+                    </span>
+                    <span class="order__title">
+                        Phong cách:
+                        <strong>Chụp ảnh giải bóng đá cấp thành</strong>
+                    </span>
+                    <span class="order__title">
+                        Tổng giá trị:
+                        <strong>2.000.000</strong>
+                        <strong>đ</strong>
+                    </span>
+                </div>
+
+                <div class="order__info">
+                    <span class="order__title">
+                        <em>Đơn đợi khách hàng thanh toán</em>
+                    </span>
+                </div>
+
+                <a href="" class="order__detail">
+                    Xem chi tiết
+                    <i class="order__detail-icon fa-solid fa-right-long"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+>>>>>>> e4098b510ef2108a428df17eaed8351ace4ad601
 </body>
 </html>
