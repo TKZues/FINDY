1bc6f6ae0516f7350b4dcd9649c88b858542a8b2
Lê Thị Mỹ Hậu <ltmyhau@gmail.com>
2023-12-28 02:44:41 +0700

    Update find-timtho.php

--- find-timtho.php
+++ find-timtho.php
@@ -293,183 +293,183 @@
 
 
 
-                                                                <?php
-                                                            }
-                                                        }
-                                                                ?>
-                                                                    </div>
-                                                                </div>
-
-                                                                <ul class="pagination">
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">
-                                                                            <i class="pagination-item__icon fa-solid fa-chevron-left"></i>
-                                                                        </a>
-                                                                    </li>
-                                                                    <li class="pagination-item pagination-item--active">
-                                                                        <a href="" class="pagination-item__link">1</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">2</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">3</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">4</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">5</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">...</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">10</a>
-                                                                    </li>
-                                                                    <li class="pagination-item">
-                                                                        <a href="" class="pagination-item__link">
-                                                                            <i class="pagination-item__icon fa-solid fa-chevron-right"></i>
-                                                                        </a>
-                                                                    </li>
-                                                                </ul>
-                                                            </div>
-                                                        </div>
-                                                </div>
-                                            </div> 
+                                    <?php
+                                        }
+                                    }
+                                    ?>
                                 </div>
                             </div>
-                            
+
+                            <ul class="pagination">
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">
+                                        <i class="pagination-item__icon fa-solid fa-chevron-left"></i>
+                                    </a>
+                                </li>
+                                <li class="pagination-item pagination-item--active">
+                                    <a href="" class="pagination-item__link">1</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">2</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">3</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">4</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">5</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">...</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">10</a>
+                                </li>
+                                <li class="pagination-item">
+                                    <a href="" class="pagination-item__link">
+                                        <i class="pagination-item__icon fa-solid fa-chevron-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+
+        </div>
+        </div>
+        </div>
+        </div>
+
+        <section>
+            <div class="footer">
+                <div class="grid wide">
+                    <div class="footer_container row">
+                        <div class="footer_logo col c-6 m-2 l-2">
+                            <div class="footer_logo-logo">
+
+                                FINDY
+                            </div>
+                            <div class="footer_logo-english">
+                                <i class="fa-solid fa-earth-asia"></i>
+                                <a href="">VietNam</a>/ <a href="">Tiếng Việt</a>
+                            </div>
+                            <div class="footer_logo-help">
+                                <i class="fa-solid fa-circle-question"></i>
+                                Giúp đỡ & Hỗ trợ
+                            </div>
+                            <div class="footer_logo-accessibility">
+                                <i class="fa-solid fa-universal-access"></i>
+                                Trợ năng
+                            </div>
+                        </div>
+                        <div class="footer_findy col c-6 m-2 l-2">
+                            <h1>Findy</h1>
+                            <ul>
+                                <a href="">
+                                    <li>Dự án</li>
+                                </a>
+                                <a href="">
+                                    <li>Cuộc thi</li>
+                                </a>
+                                <a href="">
+                                    <li>Thành viên</li>
+                                </a>
+                                <a href="">
+                                    <li>Quản lý dự án</li>
+                                </a>
+                                <a href="">
+                                    <li>Hình ảnh khắp nơi</li>
+                                </a>
+                                <a href="">
+                                    <li>Xác thực</li>
+                                </a>
+                            </ul>
+
+                        </div>
+                        <div class="footer_introduce col c-6 m-2 l-2">
+                            <h1>Giới thiệu</h1>
+                            <ul>
+                                <a href="">
+                                    <li>Về chúng tôi</li>
+                                </a>
+                                <a href="">
+                                    <li>Cách thức hoạt động</li>
+                                </a>
+                                <a href="">
+                                    <li>Bảo mật</li>
+                                </a>
+                                <a href="">
+                                    <li>Nhà đầu tư</li>
+                                </a>
+                                <a href="">
+                                    <li>Sơ đồ trang</li>
+                                </a>
+                                <a href="">
+                                    <li>Tin tức</li>
+                                </a>
+                                <a href="">
+                                    <li>Đội ngũ</li>
+                                </a>
+                                <a href="">
+                                    <li>Công việc</li>
+                                </a>
+                            </ul>
+                        </div>
+                        <div class="footer_rules col c-6 m-2 l-2">
+                            <h1>Điều khoản</h1>
+                            <ul>
+                                <a href="">
+                                    <li>Chính sách bảo mật</li>
+                                </a>
+                                <a href="">
+                                    <li>Điều khoản và điều kiện</li>
+                                </a>
+                                <a href="">
+                                    <li>Chính sách bản quyền</li>
+                                </a>
+                                <a href="">
+                                    <li>Quy tắc ứng xử</li>
+                                </a>
+                                <a href="">
+                                    <li>Các loại phí</li>
+                                </a>
+                            </ul>
+                        </div>
+                        <div class="footer_bussiness col c-6 m-2 l-2">
+                            <h1>Đối tác</h1>
+                            <ul>
+                                <a href="">
+                                    <li>Escrow.com</li>
+                                </a>
+                                <a href="">
+                                    <li>Loadshift</li>
+                                </a>
+                                <a href="">
+                                    <li>Warrior Forum</li>
+                                </a>
+                            </ul>
+                        </div>
+                        <div class="footer_app col c-6 m-2 l-2">
+                            <h1>Ứng dụng</h1>
+                            <ul>
+                                <a href="">
+                                    <li><img src="./img/app-store.svg" alt=""></li>
+                                </a>
+                                <a href="">
+                                    <li><img src="./img/google-play.svg" alt=""></li>
+                                </a>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <section>
-                <div class="footer">
-                    <div class="grid wide">
-                        <div class="footer_container row">
-                            <div class="footer_logo col c-6 m-2 l-2">
-                                <div class="footer_logo-logo">
-
-                                    FINDY
-                                </div>
-                                <div class="footer_logo-english">
-                                    <i class="fa-solid fa-earth-asia"></i>
-                                    <a href="">VietNam</a>/ <a href="">Tiếng Việt</a>
-                                </div>
-                                <div class="footer_logo-help">
-                                    <i class="fa-solid fa-circle-question"></i>
-                                    Giúp đỡ & Hỗ trợ
-                                </div>
-                                <div class="footer_logo-accessibility">
-                                    <i class="fa-solid fa-universal-access"></i>
-                                    Trợ năng
-                                </div>
-                            </div>
-                            <div class="footer_findy col c-6 m-2 l-2">
-                                <h1>Findy</h1>
-                                <ul>
-                                    <a href="">
-                                        <li>Dự án</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Cuộc thi</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Thành viên</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Quản lý dự án</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Hình ảnh khắp nơi</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Xác thực</li>
-                                    </a>
-                                </ul>
-
-                            </div>
-                            <div class="footer_introduce col c-6 m-2 l-2">
-                                <h1>Giới thiệu</h1>
-                                <ul>
-                                    <a href="">
-                                        <li>Về chúng tôi</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Cách thức hoạt động</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Bảo mật</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Nhà đầu tư</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Sơ đồ trang</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Tin tức</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Đội ngũ</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Công việc</li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <div class="footer_rules col c-6 m-2 l-2">
-                                <h1>Điều khoản</h1>
-                                <ul>
-                                    <a href="">
-                                        <li>Chính sách bảo mật</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Điều khoản và điều kiện</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Chính sách bản quyền</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Quy tắc ứng xử</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Các loại phí</li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <div class="footer_bussiness col c-6 m-2 l-2">
-                                <h1>Đối tác</h1>
-                                <ul>
-                                    <a href="">
-                                        <li>Escrow.com</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Loadshift</li>
-                                    </a>
-                                    <a href="">
-                                        <li>Warrior Forum</li>
-                                    </a>
-                                </ul>
-                            </div>
-                            <div class="footer_app col c-6 m-2 l-2">
-                                <h1>Ứng dụng</h1>
-                                <ul>
-                                    <a href="">
-                                        <li><img src="./img/app-store.svg" alt=""></li>
-                                    </a>
-                                    <a href="">
-                                        <li><img src="./img/google-play.svg" alt=""></li>
-                                    </a>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </section>
-            <script src="./find-timtho.js"></script>
+        </section>
+        <script src="./find-timtho.js"></script>
     </body>
 </php>
