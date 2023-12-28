565628f7e27b7dc2049dfdc5d5afb6aa70adbbcd
Lê Thị Mỹ Hậu <ltmyhau@gmail.com>
2023-12-28 21:04:07 +0700

    bộ lọc

--- userful/jobsearch.php
+++ userful/jobsearch.php
@@ -17,6 +17,7 @@
             color: var(--main-color);
         }
     </style>
+
 </head>
 
 <body>
@@ -28,7 +29,7 @@
         <div class="find-timtho-container">
             <div class="grid wide">
                 <div class="row find-timtho-content">
-                    <div class="col l-3 m-4 c-0">
+                    <!-- <div class="col l-3 m-4 c-0">
                         <div class="filter">
                             <h3 class="filter__heading">
                                 Lọc
@@ -203,9 +204,210 @@
                                 </div>
                             </div>
                         </div>
+                    </div> -->
+
+                    <div class="col l-12 m-12 c-0">
+                        <div class="filter">
+                            <div class="filter__list">
+                                <div class="filter__heading">
+                                    <i class="filter__heading-icon fa-solid fa-filter"></i>
+                                    Lọc
+                                </div>
+                                <div class="filter__item-warp">
+                                    <div id="filter__place-item" class="filter__item">
+                                        Địa điểm
+                                        <i id="filter__place-icon-up" class="filter__item-icon fa-solid fa-chevron-up" style="display: none;"></i>
+                                        <i id="filter__place-icon-down" class="filter__item-icon fa-solid fa-chevron-down"></i>
+                                    </div>
+                                    <!-- <div id="filter__place-menu" class="filter__menu" style="display: none;">
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-1" class="">
+                                            <label for="place-1" class="filter__menu-name">Hồ Chí Minh</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-2" class="">
+                                            <label for="place-2" class="filter__menu-name">Hà Nội</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-3" class="">
+                                            <label for="place-3" class="filter__menu-name">Đà Nẵng</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-4" class="">
+                                            <label for="place-4" class="filter__menu-name">Đà Lạt</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-5" class="">
+                                            <label for="place-5" class="filter__menu-name">Phú Quốc</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="place-6" class="">
+                                            <label for="place-6" class="filter__menu-name">Khác</label>
+                                        </div>
+                                    </div> -->
+                                </div>
+                                <div class="filter__item-warp">
+                                    <div id="filter__price-item" class="filter__item">
+                                        Giá
+                                        <i id="filter__price-icon-up" class="filter__item-icon fa-solid fa-chevron-up" style="display: none;"></i>
+                                        <i id="filter__price-icon-down" class="filter__item-icon fa-solid fa-chevron-down"></i>
+                                    </div>
+                                    <div id="filter__price-menu" class="filter__menu" style="display: none;">
+                                        <div class="filter__menu-item">
+                                            <input type="text" name="" id="" class="filter__price-input" placeholder="₫ TỪ">
+                                            <span class="filter__menu-name">-</span>
+                                            <input type="text" name="" id="" class="filter__price-input" placeholder="₫ ĐẾN">
+                                        </div>
+                                        <button class="btn btn--primary filter__menu-btn">ÁP DỤNG</button>
+                                    </div>
+                                </div>
+                                <div class="filter__item-warp">
+                                    <div id="filter__experience-item" class="filter__item">
+                                        Kinh nghiệm
+                                        <i id="filter__experience-icon-up" class="filter__item-icon fa-solid fa-chevron-up" style="display: none;"></i>
+                                        <i id="filter__experience-icon-down" class="filter__item-icon fa-solid fa-chevron-down"></i>
+                                    </div>
+                                    <div id="filter__experience-menu" class="filter__menu" style="display: none;">
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="exp-1" class="">
+                                            <label for="exp-1" class="filter__menu-name">Dưới 1 năm</;>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="exp-2" class="">
+                                            <label for="exp-2" class="filter__menu-name">1 - 3 năm</;>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="exp-3" class="">
+                                            <label for="exp-3" class="filter__menu-name">3 - 5 năm</;>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="exp-4" class="">
+                                            <label for="exp-4" class="filter__menu-name">Trên 5 năm</;>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="filter__item-warp">
+                                    <div id="filter__skill-item" class="filter__item">
+                                        Kỹ năng
+                                        <i id="filter__skill-icon-up" class="filter__item-icon fa-solid fa-chevron-up" style="display: none;"></i>
+                                        <i id="filter__skill-icon-down" class="filter__item-icon fa-solid fa-chevron-down"></i>
+                                    </div>
+                                    <div id="filter__skill-menu" class="filter__menu" style="display: none;">
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="skill-1" class="">
+                                            <label for="skill-1" class="filter__menu-name">Chụp ảnh</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="skill-2" class="">
+                                            <label for="skill-2" class="filter__menu-name">Quay video</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="skill-3" class="">
+                                            <label for="skill-3" class="filter__menu-name">Edit ảnh</label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="skill-4" class="">
+                                            <label for="skill-4" class="filter__menu-name">Edit video</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="filter__item-warp">
+                                    <div id="filter__rating-item" class="filter__item">
+                                        Đánh giá
+                                        <i id="filter__rating-icon-up" class="filter__item-icon fa-solid fa-chevron-up" style="display: none;"></i>
+                                        <i id="filter__rating-icon-down" class="filter__item-icon fa-solid fa-chevron-down"></i>
+                                    </div>
+                                    <div id="filter__rating-menu" class="filter__menu" style="display: none;">
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-1" class="">
+                                            <label for="rating-1" class="star-rating">
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-2" class="">
+                                            <label for="rating-2" class="star-rating">
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-3" class="">
+                                            <label for="rating-3" class="star-rating">
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-4" class="">
+                                            <label for="rating-4" class="star-rating">
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-5" class="">
+                                            <label for="rating-5" class="star-rating">
+                                                <i class="star-rating--gold fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                        <div class="filter__menu-item">
+                                            <input type="checkbox" id="rating-6" class="">
+                                            <label for="rating-6" class="star-rating">
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="filter__menu-warp">
+                                <div id="filter__place-menu" class="filter__menu" style="display: none;">
+                                    <div class="btn filter__menu-item">
+                                        Hồ Chí Minh
+                                    </div>
+                                    <div class="btn filter__menu-item">
+                                        Hà Nội
+                                    </div>
+                                    <div class="btn filter__menu-item">
+                                        Đà Nẵng
+                                    </div>
+                                    <div class="btn filter__menu-item">
+                                        Đà Lạt
+                                    </div>
+                                    <div class="btn filter__menu-item">
+                                        Phú Quốc
+                                    </div>
+                                    <div class="btn filter__menu-item">
+                                        Khác
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div class="col l-9 m-8 c-12">
+                    <div class="col l-12 m-12 c-12">
                         <div class="freelancer">
                             <?php
                             include "../connect.php"
@@ -229,7 +431,7 @@
                                         $_SESSION['gioithieu'] = $result['gioithieu'];
                                 ?>
 
-                                        <div class="col l-4 m-6 c-12">
+                                        <div class="col l-3 m-4 c-12">
                                             <div class="freelancer-item">
                                                 <a href="info-freelancer.php?name=<?php echo urlencode($result['ten']); ?>&ma_profile=<?php echo urlencode($result['ma_profile']); ?>&job=<?php echo urlencode($result['nghenghiep']); ?>&rating=4.9&reviews=<?php echo urlencode($result['gioithieu']); ?>&sdt=<?php echo urlencode($result['sdt']); ?>&email=<?php echo urlencode($result['email']); ?>&address=<?php echo urlencode($result['diachi']); ?>" class="freelancer-item-warp">
                                                     <div class="freelancer-item__personal-info">
@@ -460,8 +662,9 @@
 
     </section> -->
 
-    <!-- <script src="../find-timtho.js"></script> -->
+    <script src="../find-timtho.js"></script>
 
+    
     <?php
     include "../footer.php"
     ?>
